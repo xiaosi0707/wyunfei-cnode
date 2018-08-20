dc450fa88d343cc2778a7ba1b81cb7463db9fc9e
wyunfei <583514368@qq.com>
2018-08-20 17:01:44 +0800

    详情页

--- src/view/detail/index.js
+++ src/view/detail/index.js
@@ -1,9 +1,31 @@
 /*Created by SmallFour on 2018/8/16*/
 import React, { Component } from 'react'
-
+import { Card, Tag, Avatar } from 'antd'
+import {Link} from 'react-router-dom'
+import TitleTag from '../common/tag'
+import data from './data'
 class Detail extends Component {
+    constructor(props) {
+        super(props)
+    }
     render() {
-        return '详情'
+        const title = (<div>
+            <h1>{data.data.title}</h1>
+            <div>
+                <TitleTag data={data.data}></TitleTag>
+                <Avatar src={data.data.author.avatar_url}></Avatar>
+                <Link to={'/user/' + data.data.author.loginname}>{data.data.author.loginname}</Link>
+                发表于{data.data.create_at.split('T')[0]}
+            </div>
+        </div>)
+        return (
+            <div className='wrap'>
+                <Card
+                title={title}>
+                    <div dangerouslySetInnerHTML={{ __html: data.data.content }}></div>
+                </Card>
+            </div>
+        )
     }
 }
 
