b99fe647cabe0b629162076e9b43428d9e0f53b5
wyunfei <583514368@qq.com>
2018-08-20 19:31:37 +0800

    用户详情 - 头部处理

--- src/view/user/index.js
+++ src/view/user/index.js
@@ -1,9 +1,27 @@
 /*Created by SmallFour on 2018/8/16*/
 import React, { Component } from 'react'
+import { Avatar, Row, Col } from 'antd'
+import data from './data'
 
 class User extends Component {
     render() {
-        return '用户详情'
+        let { avatar_url, loginname, score, create_at } = data.data
+        return (
+            <div className='wrap'>
+                <Avatar src={avatar_url} className='user-avatar'/>
+                <Row className='user-info'>
+                    <Col md={8}>
+                        用户名：{ loginname }
+                    </Col>
+                    <Col md={8}>
+                        积分：{ score }
+                    </Col>
+                    <Col md={8}>
+                        注册时间：{ create_at.split('T')[0] }
+                    </Col>
+                </Row>
+            </div>
+        )
     }
 }
 
